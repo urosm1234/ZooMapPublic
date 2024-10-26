eb358689fe47db935d22e0bd303bbb43985a76b5
urosm1234 <uki.mar@gmail.com>
2024-10-26 21:34:22 +0200

    Update Program.cs

--- ZooMapProject/Program.cs
+++ ZooMapProject/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
     options.Cookie.Name = "Login.Session";
-    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
     options.Cookie.IsEssential = true;
 });
 var app = builder.Build();
@@ -22,12 +22,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStaticFiles();
+
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseSession();
-
 app.UseRouting();
 
 app.MapControllerRoute(
