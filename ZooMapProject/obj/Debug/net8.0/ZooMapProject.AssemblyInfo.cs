611f0f5c216effc041f6136e0090bd263564ee42
urosm1234 <uki.mar@gmail.com>
2024-10-27 20:45:36 +0100

    Removed Navbar

--- ZooMapProject/obj/Debug/net8.0/ZooMapProject.AssemblyInfo.cs
+++ ZooMapProject/obj/Debug/net8.0/ZooMapProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooMapProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533215f3cc9a637afbd64c61827f35546df9f0ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+108b5e256421c88f5787d84986d4ccd40cc8b9cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooMapProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooMapProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
