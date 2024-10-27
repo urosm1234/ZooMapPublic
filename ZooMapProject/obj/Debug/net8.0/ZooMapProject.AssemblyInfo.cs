214e822fff17a05f5157bb11b290010e6129e9a0
urosm1234 <uki.mar@gmail.com>
2024-10-27 22:14:13 +0100

    Dodate zivotinje i vektorska mapa

--- ZooMapProject/obj/Debug/net8.0/ZooMapProject.AssemblyInfo.cs
+++ ZooMapProject/obj/Debug/net8.0/ZooMapProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooMapProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+108b5e256421c88f5787d84986d4ccd40cc8b9cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b1cbdf73254f426963e7373416888cc54122d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooMapProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooMapProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
