36bab184df7ad807df6d916688295b3e43a6c658
urosm1234 <uki.mar@gmail.com>
2024-10-30 17:47:45 +0100

    Added more animals

--- ZooMapProject/obj/Debug/net8.0/ZooMapProject.AssemblyInfo.cs
+++ ZooMapProject/obj/Debug/net8.0/ZooMapProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooMapProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+214e822fff17a05f5157bb11b290010e6129e9a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699c5021b93b97fc6c22b91ea0581e345b73a0ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooMapProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooMapProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
