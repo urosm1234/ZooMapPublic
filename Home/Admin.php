7fa7e9cc0cb04666388405367dc64dd0ed7d6f28
urosm1234 <uki.mar@gmail.com>
2025-05-08 21:28:35 +0200

    Styled Admin page

--- Home/Admin.php
+++ Home/Admin.php
@@ -22,16 +22,22 @@ if(!isset($_SESSION['user']))
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "stylesheet" href = "Admin.css">
+
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
     <!-- Include Leaflet JS -->
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
     <script src = "<?php echo $PATH?>js/shared.js"></script>
     <script src = "<?php echo $PATH?>js/admin.js"></script>
-    <script src = "<?php echo $PATH?>js/sidebar.js"></script>
+
     <title>Text Input Form</title>
     <style>
                     :root {
         color-scheme: only light;
+        }
+        #map{
+            width:100%;
+            height:95vh;
+            overflow-y:hidden;
         }
         .form-wrapper{
             display:flex;
@@ -118,6 +124,35 @@ if(!isset($_SESSION['user']))
             border-radius:15px;
             text-decoration: none;
         }
+        .sidebar {
+        height: 100%;
+        position: fixed;
+        top: 0;
+        right: 0;
+        background-color: #f4f4f4;
+        overflow-x: hidden;
+        transition: 0.3s;
+        padding: 30px;
+        width: 500px;
+        box-shadow: -2px 0 5px rgba(0,0,0,0.1);
+        display:none;
+        }
+        .sidebar.open {
+            display:block;
+        }
+        .toggle-button {
+        position: fixed;
+        top: 20px;
+        right: 20px;
+        z-index: 1001;
+        padding: 10px 15px;
+        background-color: #007BFF;
+        color: white;
+        border: none;
+        cursor: pointer;
+        border-radius: 4px;
+        }
+
     </style>
     <script>
         
@@ -136,6 +171,7 @@ if(!isset($_SESSION['user']))
                 var icont = new AnimalIcon({iconUrl: PATH +'images/new_icons/'+icon.name+'.png'});
                 var marker = L.marker([icon.coordinateh, icon.coordinatew], { icon: icont, draggable:true }).addTo(map)
                 .on('click',()=> fillInputsWithAnimalInfo(icon.id, icon.id-1,iconArray ) )
+                .on('click',()=> toggleSidebar(ifClosed = true) )
                 .on('mouseup', (event)=> updateCoords(event, icon.id));
                 console.log(icon.name)
                 console.log(icon.id-1);
@@ -233,58 +269,80 @@ if(!isset($_SESSION['user']))
         //else console.log(title + latin_title + red + porodica + staniste + zivotni_vek + rasprostranjenost + klasa + endangered_level + tekst)
         return 1;
     }
+
+    function toggleSidebar(ifClosed) {
+    const sidebar = document.getElementById("sidebar");
+    if(ifClosed && sidebar.classList.contains("open"))
+        return
+    sidebar.classList.toggle("open");
+  }
     </script>
 </head>
 <body>
     <form>
     <a id="logout-button" href = "./Requests/logout.php" type = "submit">Logout</a>
     </form>
-    <div id ="map"></div>
+
+    <div id ="map" ></div>
+
+    <button class="toggle-button" onclick="toggleSidebar(false)">Toggle Form</button>
+
+    <div id = "sidebar"class="sidebar">
+
     <h1>Input Form</h1>
     <div class = "form-wrapper">
     <form method="put" action="/api/Updates/2">
         <label for="Id">Animal Id:</label>
         <input id="Id" type="number" name="id" class="small" style="background-color:#ccc" readonly/>
-
+        <br>
         <label for="smallInput1">Naziv životnije:</label>
         <input id="smallInput1" type="text" name="title" class="medium" />
-
+        <br>
         <label for="smallInput2">Naziv na latinskom:</label>
         <input id="smallInput2" type="text" name="latin_title" class="medium" />
-
+        <br>
         <label for="smallInput3">Red:</label>
         <input id="smallInput3" type="text" name="red" class="medium" />
-
+        <br>
         <label for="smallInput4">Porodica:</label>
         <input id="smallInput4" type="text" name="porodica" class="medium" />
-
+        <br>
         <label for="mediumInput1">Stanište:</label>
         <textarea id="mediumInput1" type="text" name="staniste" class="medium" ></textarea>
-
+        <br>
         <label for="mediumInput2">Životni vek:</label>
         <textarea id="mediumInput2" type="text" name="zivotni_vek" class="medium" ></textarea>
-
+        <br>
         <label for="mediumInput3">Rasprostranjenost:</label>
         <textarea id="mediumInput3" type="text" name="rasprostranjenost" class="medium" ></textarea>
-
+        <br>
         <label for="smallInput5">Klasa:</label>
         <input id="smallInput5" type="text" name="klasa" class="medium" />
-
+        <br>
         <label for="smallInput6">ENDANGERED LEVEL:</label>
         <input id="smallInput6" type="text" name="endangered_level" class="medium" />
-
+        <br>
         <label for="largeInput">Tekst:</label>
         <textarea id="largeInput" name="largeInput" class="large"></textarea>
-
+        <br>
         <button type="button" onclick = "formSubmitted()">Submit</button>
-        <dspan id ="responseText"></span>
+        <span id ="responseText"></span>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
     </form>
+    </div>
     </div>
 
 
 
-
 <script>
+
 
     var map = L.map('map', {
         minZoom: -1,  //44.824739, 20.452049 zoo corner
--- js/shared.js
+++ js/shared.js
@@ -96,15 +96,15 @@ function changeView(animal_id, iconArray) {
 
             animal_title_h_tags[0].innerHTML = iconArray[animal_id].title;
             animal_title_h_tags[1].innerHTML= iconArray[animal_id].latin_title;
-            if(iconArray[animal_id].endangered_level != null)
-            {
+            try{
                 console.log(iconArray[animal_id].endangered_level)
                 let endangered_levels = {"CR":["Critically endangered", "red"], "EN":["Endangered","orange"], "LC":["Least concern","green"],"VU":["Vulnerable", "#FFBF00"], "NT":["Near threatened", "#8A9A5B"]}
                 animal_title_h_tags[2].style.color = endangered_levels[ iconArray[animal_id].endangered_level ][1];
                 animal_title_h_tags[2].innerHTML = " "+iconArray[animal_id].endangered_level + " - "+ endangered_levels[ iconArray[animal_id].endangered_level ][0];
             }
-            else 
+            catch{
             document.getElementById("animal-title").getElementsByTagName("h")[2].innerHTML = ""
+            }
 
             let targetParagraph = document.getElementById("description").children[0];
             let content = "";
